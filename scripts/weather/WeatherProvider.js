01fadf4a0b333fe3c244c41e236838d418586f13
heymonicakay <hey.monicakay@gmail.com>
2020-08-06 13:43:20 -0500

    minor changes

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,21 +1,19 @@
-import { keys } from "../Settings.js"
+import { keys } from "../Settings.js";
 
 const eventHub = document.querySelector(".container")
-let weather = [] 
+
+let weather = []
       
 
-export const useWeather = () => weather.slice()
-      
-
-export const getWeather = () => {
-      return fetch("http://api.openweathermap.org/data/2.5/forecast?q=${nashville}&cnt=48&units=imperial&appid=20a56fc71e8cb7dbfebfd4cd961766af")
+export const getWeather = ( parkObj ) => {
+      return fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=${parkObj.postalCode}&cnt=48&units=imperial&appid=20a56fc71e8cb7dbfebfd4cd961766af`)
             .then(response => response.json())
             .then(parsedData => {
                   weather = parsedData
             })
 }
-   
 
-// listens for 
+export const useWeather = () => weather.slice()
+
 
       
