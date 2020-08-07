37b35a0cd8b729a17cc16be1b59cc6ccdcc65227
justha <nganha42@gmail.com>
2020-08-07 10:35:20 -0500

    styled existing components and save button dispatch

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ import { EateryList } from "./eateries/EateryList.js";
 import { EaterySelect } from "./eateries/EaterySelect.js";
 import { ParkList } from "./parks/ParkList.js";
 import { ParkSelect } from "./parks/ParkSelect.js";
+import { renderSaveButton } from "./itinerary-preview/ItineraryPreview.js";
 
 BizList()
 BizSelect()
@@ -11,3 +12,4 @@ EateryList()
 EaterySelect()
 ParkList()
 ParkSelect()
+renderSaveButton()
