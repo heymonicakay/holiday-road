6628cc45dc9d2b659dc372e9a7126e0886386f5d
justha <nganha42@gmail.com>
2020-08-07 14:19:20 -0500

    commit before reviewing art-safety

--- scripts/.Settings.js
+++ scripts/.Settings.js
@@ -1,6 +1,8 @@
-export default {
+// no files are importing from .Settings.js
+
+export const keys {
 	graphhopperKey: "a79ca1c7-acf5-4c95-b13b-29d4b5c07c21",
 	npsKey: "hqFcQBZOdKmyA7kliNAo2cikzMcEnuweR1JRxqnq",
-	weatherKey: "20a56fc71e8cb7dbfebfd4cd961766af"
+	"weatherKey": "20a56fc71e8cb7dbfebfd4cd961766af"
 }
 
