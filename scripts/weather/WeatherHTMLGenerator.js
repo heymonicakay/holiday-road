bcb40d7f169a7a4f3491a02e3ab164e6205b9ada
ardisrtilson <email@example.com>
2020-08-07 14:10:03 -0500

    Added weather modules.

--- scripts/weather/WeatherHTMLGenerator.js
+++ scripts/weather/WeatherHTMLGenerator.js
@@ -1,4 +1,13 @@
-// generates HTML to display individual weather object 
-
-// exports WeatherHTML()
-
+export const ForecastHTML = (weather) => {
+    return `
+          <section class="forecast-card">
+                Timestamp: ${weather[0].dt}
+                <div class="container--weather-image">
+                      <img class="image weather-image" src="http://openweathermap.org/img/wn/${weather[0].weather[0].icon}@2x.png">
+                </div>
+                <div class="forecast--temp">
+                      ${weather[0].main.temp}&deg; F
+                </div>
+          </section>
+    `
+}
