1447d5c3cc0b700c18a026b971626327d6535c07
justha <nganha42@gmail.com>
2020-08-10 09:22:03 -0500

    commit before viewing art-saveButton

--- scripts/parks/ParkHTMLGenerator.js
+++ scripts/parks/ParkHTMLGenerator.js
@@ -1,7 +1,8 @@
 export const ParkHTML = (parkObj) => {
       return `
-            <section>
+      <section class="preview--park">
                   <div>${parkObj.fullName}</div>
+                  <button class="button--details-park" id="button__park--${parkObj.id}">Park Details</button>
             </section>
       `
 }
