b206dafdec513a77a6bd99c32b6230c9924cd926
heymonicakay <hey.monicakay@gmail.com>
2021-04-19 03:01:49 -0500

    fixed itin render

--- scripts/parks/ParkProvider.js
+++ scripts/parks/ParkProvider.js
@@ -1,10 +1,11 @@
-import { keys } from "../Settings.js"
 
 let parksArray = []
 
 export const useParks = () => [...parksArray]
 
-
-export const getParks = () => fetch(`https://developer.nps.gov/api/v1/parks?api_key=${keys.npsKey}`)
-    .then(res => res.json())
-    .then(data => parksArray = data.data);
+export const getParks = () =>
+  fetch(
+    `https://developer.nps.gov/api/v1/parks?api_key=hqFcQBZOdKmyA7kliNAo2cikzMcEnuweR1JRxqnq`
+  )
+    .then((res) => res.json())
+    .then((data) => (parksArray = data.data))
--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,4 +1,3 @@
-import { keys } from "../Settings.js"
 import { useParks } from "../parks/ParkProvider.js"
 
 let forecast = {}
@@ -9,21 +8,23 @@ export const getZip = (parkObj) => {
 }
 
 export const getWeatherData = ( zip ) => {
-    return fetch(`http://api.openweathermap.org/data/2.5/forecast/?zip=${zip}&units=imperial&appid=${keys.weatherKey}`)
-    .then(res => res.json())
-    .then(data => {
+    return fetch(
+      `http://api.openweathermap.org/data/2.5/forecast/?zip=${zip}&units=imperial&appid=20a56fc71e8cb7dbfebfd4cd961766af`
+    )
+      .then((res) => res.json())
+      .then((data) => {
         const fiveDay = []
-        const arr = data.list;
+        const arr = data.list
 
-        for(let i = 0; i < arr.length; i++){
-            if(i === 0 || i === 7 || i === 15 || i === 23 || i === 31 ){
-                fiveDay.push(arr[i]);
-            }
+        for (let i = 0; i < arr.length; i++) {
+          if (i === 0 || i === 7 || i === 15 || i === 23 || i === 31) {
+            fiveDay.push(arr[i])
+          }
         }
 
-        forecast.city = data.city.name;
+        forecast.city = data.city.name
         forecast.fiveDay = fiveDay
 
-        return forecast;
-    })
+        return forecast
+      })
 }
